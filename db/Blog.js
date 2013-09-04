dde94c6d09e181e1919b4da412ef06ce6f1aac4a
xi.he <xi.he@baidao.com>
2013-09-04 18:02:06 +0800

    first commit

---
+++ db/Blog.js
@@ -0,0 +1,84 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+var db = mongoose.connection;
+db.on('error',function(){
+  console.log('connection error');
+});
+db.on('open', function(){
+  console.log('connection open');
+})
+
+var blogSchema = mongoose.Schema({
+  title: String,
+  content: String,
+  author: String,
+  createTime: {type: Date, default: Date.now}
+});
+var Blog = mongoose.model('Blog', blogSchema);
+
+exports.create = function(title, content, author, options){
+  isNotBlank([
+    {'content':title, 'message':'title is empty!'},
+    {'content':content, 'message':'content is empty!'},
+    {'content':author, 'message':'author is empty!'}
+  ]);
+  var blog = new Blog(
+    {
+      'title': title,
+      'content': content,
+      'author': author
+    }
+  )
+  blog.save(function(error,blog){
+    handleQueryResult(error, blog, options);
+  })
+};
+
+exports.findAll = function(options){
+  Blog.find(function(error, blogs){
+    handleQueryResult(error, blogs, options);
+  });
+};
+
+exports.update = function(id, title, content, options){
+  isNotBlank([{'content': id, 'message': 'id is empty!'}]);
+  var updateObj ={};
+  if(title && title !== ''){
+    updateObj.title = title;
+  }
+  if(content && content !== ''){
+    updateObj.content = content;
+  }
+  console.log('updateObj--->', updateObj);
+  Blog.findByIdAndUpdate(id, updateObj, function(error, blog){
+    handleQueryResult(error, blog, options);
+  })
+};
+
+exports.delete = function(id, options){
+  isNotBlank([{'content': id, 'message': 'id is empty!'}]);
+  Blog.findByIdAndRemove(id, function(error, blog){
+    handleQueryResult(error, blog, options);
+  });
+};
+
+function isNotBlank(holder){
+  var _temp;
+  for (var i in holder) {
+    _temp = holder[i];
+    if(!_temp.content || _temp.content === ''){
+      throw Error(_temp.message);
+    }
+  };
+}
+
+function handleQueryResult(error, result, options){
+  if(error){
+    console.log(error);
+    if(options.error){
+      options.error(error);
+    }
+  }else{
+    options.success(result);
+  }
+}
---
+++ server.js
@@ -0,0 +1,50 @@
+var restify = require('restify');
+var Blog = require('./db/Blog');
+var server = restify.createServer({
+  name:'YtxServer'
+});
+server.use(restify.bodyParser());
+server.post('/blog', function create(req, res, next){
+  Blog.create(req.params.title, req.params.content, req.params.author, {
+    success: function(blog){
+      res.end("create a blog success!");
+    }
+  })
+});
+
+server.get('/blogs', function(req, res, next){
+  Blog.findAll({
+    error: function(error){
+      res.end(500,error);
+    },
+    success:function(blogs){
+      res.writeHead(200, {
+        'Content-Type': 'application/json'
+      });
+      res.end(JSON.stringify(blogs));
+    }
+  })
+});
+server.put('/blog/:id', function(req, res, next){
+  Blog.update(req.params.id, req.params.title, req.params.content, {
+    error: function(error){
+      res.end(500,error);
+    },
+    success:function(blog){
+      res.writeHead(200, {
+        'Content-Type': 'application/json'
+      });
+      res.end(JSON.stringify(blog));
+    }
+  });
+});
+server.del('/blog/:id', function(req, res, next){
+  Blog.delete(req.params.id, {
+    success: function(blog){
+      res.end('success to delete the blog!');
+    }
+  });
+});
+server.listen(8083, function(){
+  console.log('%s listening at %s', server.name, server.url);
+})
